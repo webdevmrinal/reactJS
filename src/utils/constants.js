035fcdddb0e66d26570c37cf25fbaba488fb7c65
webdevmrinal <mrinal.webdev@gmail.com>
2023-05-05 22:25:37 +0530

    added cors-anywhere proxy to the api link for resolving cors error

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,7 +4,8 @@ export const CAROUSEL_CARD_IMAGE = "https://res.cloudinary.com/swiggy/image/uplo
 
 export const RESTAURANT_CARD_IMAGE = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/"
 
-export const RESTAURANT_LIST_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.6724138&lng=86.1527555&page_type=DESKTOP_WEB_LISTING"
+export const RESTAURANT_LIST_URL = "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.45133957193621&lng=78.39913438366459&page_type=DESKTOP_WEB_LISTING"
+// export const RESTAURANT_LIST_URL = "http://localhost:3001/api/restaurants?lat=23.6724138&lng=86.1527555&page_type=DESKTOP_WEB_LISTING"
 
 export const FETCH_MORE_RESTAURANTS = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.6724138&lng=86.1527555&offset=15&sortBy=RELEVANCE&pageType=SEE_ALL&page_type=DESKTOP_SEE_ALL_LISTING"
 // export const RESTAURANT_LIST_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.45133957193621&lng=78.39913438366459&page_type=DESKTOP_WEB_LISTING"
