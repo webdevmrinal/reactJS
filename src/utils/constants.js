b7323c638305f915bd802f1a7b437666ab9d0f57
webdevmrinal <mrinal.webdev@gmail.com>
2023-05-13 10:53:05 +0530

    added _redirects file to solve CORS

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,7 +4,12 @@ export const CAROUSEL_CARD_IMAGE = "https://res.cloudinary.com/swiggy/image/uplo
 
 export const RESTAURANT_CARD_IMAGE = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/"
 
-export const RESTAURANT_LIST_URL = "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.45133957193621&lng=78.39913438366459&page_type=DESKTOP_WEB_LISTING"
+const PROXY_URL= "https://cors-anywhere.herokuapp.com/"
+const SWIGGY_API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.45133957193621&lng=78.39913438366459&page_type=DESKTOP_WEB_LISTING"
+
+// export const RESTAURANT_LIST_URL = PROXY_URL+SWIGGY_API
+export const RESTAURANT_LIST_URL = "/api?lat=17.45133957193621&lng=78.39913438366459&page_type=DESKTOP_WEB_LISTING";
+
 // export const RESTAURANT_LIST_URL = "http://localhost:3001/api/restaurants?lat=23.6724138&lng=86.1527555&page_type=DESKTOP_WEB_LISTING"
 
 export const FETCH_MORE_RESTAURANTS = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.6724138&lng=86.1527555&offset=15&sortBy=RELEVANCE&pageType=SEE_ALL&page_type=DESKTOP_SEE_ALL_LISTING"
